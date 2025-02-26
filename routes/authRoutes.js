4580507ef2c2db56bac55bb68dd3c7c6dec64e41
youssef <matoussiyoussef28@gmail.com>
2025-02-26 17:31:09 +0100

    Fix: authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authcontroller');
 const router = express.Router();
 
 router.post('/register', authController.register);
